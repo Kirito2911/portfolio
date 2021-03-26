29ffac44cb831a8f3c962192d3f0fdf744b8c821
Jean Brito <jean_carlo291@hotmail.com>
2021-03-25 20:33:57 -0400

    sera2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Blocks } from './components/Blocks';
 import { Header } from './components/Header';
-import { Home } from './components/Home';
+
 
 import './styles/Global.css';
 function App() {
